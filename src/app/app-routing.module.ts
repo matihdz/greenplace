0df2d8f9a87db011cb0cd509069ddee40affd6b2
Yugoxc <dee1222002@gmail.com>
2022-06-19 23:36:11 -0400

    Estructura login funcionalidad Base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,15 @@ const routes: Routes = [
     path: 'tab4',
     loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
   },
+  {
+    path: 'register',
+    loadChildren: () => import('./access/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./access/login/login.module').then( m => m.LoginPageModule)
+  }
+
 ];
 @NgModule({
   imports: [
